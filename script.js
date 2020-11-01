7c1cb6a2c5ef10d2fb783a3f4bee10bcec505029
JamesBustamante <jamesbustamante99@gmail.com>
2020-11-01 18:45:08 +1100

    individual items can be removed from the app

--- script.js
+++ script.js
@@ -2,35 +2,92 @@
 var todoList = document.getElementById('todo-list');
 var addItemBtn = document.getElementById('add-item');
 var inputBox = document.getElementById('input-task');
+var removeItemBtn = document.getElementById('remove-item');
+var updateItemBtn = document.getElementById('update-item');
+
+
+// Handle the removing of elements
+var binClassBtn = document.getElementsByClassName('fa fa-trash');
+handleHideTasks();
+function handleHideTasks() {
+    var i;
+    for (i = 0; i < binClassBtn.length; i++) {
+        binClassBtn[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+console.log(binClassBtn)
+
+removeItemBtn.addEventListener('click', function () {
+    var firstElement = todoList.firstElementChild;
+
+    todoList.removeChild(firstElement);
+});
+
+updateItemBtn.addEventListener('click', function () {
+    var firstElement = todoList.firstElementChild;
+    updatedNode = createNewNode();
+
+    if (updatedNode != null || updatedNode != undefined) //Error handling
+        todoList.replaceChild(updatedNode, firstElement);
+    handleHideTasks();
+});
 
 // Add an event listener to button : press enter
-inputBox.addEventListener('keyup', function (e) { 
-    if (e.keyCode === 13) { 
+inputBox.addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) {
         addListItem();
     }
 });
 
-
 // Add an event listener to button : onclick
 addItemBtn.addEventListener('click', addListItem);
 
-// Create new tasks based on input entered into textbox
-function addListItem() { 
+function createNewNode() {
     var inputBox = document.getElementById('input-task');
-    console.log("here");
-    if (inputBox.value.length <= 0) { 
+    if (inputBox.value.length <= 0) {
         alert('Please enter the name of the task')
         return;
     }
 
     var count = todoList.childElementCount;
     var newListElement = document.createElement('li');
+    var newIdiomElement = document.createElement('i');
+
     // Manipulate the DOM
     newListElement.appendChild(document.createTextNode(inputBox.value));
+    newListElement.appendChild(newIdiomElement);
     // Change element properties
-    newListElement.id = "list-item-" + (count + 1); 
+    newListElement.id = "list-item-" + (count + 1);
     newListElement.className = "list-group-item";
-    console.log(newListElement); // Print to console
+    newIdiomElement.className = "fa fa-trash";
+
+    return newListElement;
+}
+
+// Create new tasks based on input entered into textbox
+// TODO - Refactor function - use createNewNode
+function addListItem() {
+    var inputBox = document.getElementById('input-task');
+    if (inputBox.value.length <= 0) {
+        alert('Please enter the name of the task')
+        return;
+    }
+
+    var count = todoList.childElementCount;
+    var newListElement = document.createElement('li');
+    var newIdiomElement = document.createElement('i');
+
+    // Manipulate the DOM
+    newListElement.appendChild(document.createTextNode(inputBox.value));
+    newListElement.appendChild(newIdiomElement);
+    // Change element properties
+    newListElement.id = "list-item-" + (count + 1);
+    newListElement.className = "list-group-item";
+    newIdiomElement.className = "fa fa-trash";
 
     // Insert at the beginning at the list
     //var firstItem = todoList.firstElementChild;
@@ -38,4 +95,6 @@ function addListItem() {
     todoList.appendChild(newListElement); // append to todo list
 
     inputBox.value = "";
+
+    handleHideTasks();
 }
